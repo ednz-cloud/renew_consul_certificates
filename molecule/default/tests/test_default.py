e413cac4ae0d2685514ea0bd389d5ef59bca3659
ext.blanson <ext.blanson@cma-cgm.com>
2023-04-28 00:04:26 +0200

    skeleton

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,9 @@
+"""Role testing files using testinfra."""
+
+
+def test_hosts_file(host):
+    """Validate /etc/hosts file."""
+    etc_hosts = host.file("/etc/hosts")
+    assert etc_hosts.exists
+    assert etc_hosts.user == "root"
+    assert etc_hosts.group == "root"
